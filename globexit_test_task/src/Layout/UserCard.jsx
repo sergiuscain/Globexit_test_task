9c2de19d56bdc16f46bdf9660837ebd21f798ed7
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-04-07 12:14:55 +0300

    Добавил значки

--- globexit_test_task/src/Layout/UserCard.jsx
+++ globexit_test_task/src/Layout/UserCard.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const UserCard = (props) => {
-    return(
+    return (
         <div className="employee-card" key={props.data.email}>
-            <h5 className="employee-name">{props.data.name}</h5>
-            <h5 className="employee-phone">{props.data.phone}</h5>
-            <h5 className="employee-email">{props.data.email}</h5>
+            <h3 className="employee-name">{props.data.name}</h3>
+            <h5 className="employee-phone">
+                <FontAwesomeIcon icon={faPhone} className="icon" /> {props.data.phone}
+            </h5>
+            <h5 className="employee-email">
+                <FontAwesomeIcon icon={faEnvelope} className="icon" /> {props.data.email}
+            </h5>
         </div>
     )
 }
